d3c5b4e43705839cf0f42c8cbb7f7593eb2289eb
spacehoboguy <ph.olafsen@gmail.com>
2023-10-22 13:47:41 +0200

    fix: overriding post creator when new post added

--- controller/blogs.js
+++ controller/blogs.js
@@ -56,19 +56,12 @@ blogsRouter.post('/', async (request, response) => {
 blogsRouter.put('/:id', async (request, response) => {
     const body = request.body
 
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: 'token invalid' })
-    }
-
-    const user = await User.findById(decodedToken.id)
 
     const blog = {
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
-        user: user.id
     }
 
     const updatedBlog =
